ee56b5aa2d489bd338dc8929ec8e22490994d09a
Fernanda Reis Kennedy <fernandareisdp@gmail.com>
2023-09-24 13:50:17 -0400

    adding account routes and readme

---
+++ back-end/routes/account.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+
+const {
+  createAccount,
+  deleteAccountById,
+  updateAccountById,
+  getAccountByEmail,
+} = require("../controllers/account");
+
+//POST for creating profile
+router.post("/signup", createAccount);
+
+//PUT updates profile
+router.put("/:id", updateAccountById);
+
+//Delete removes a profile
+router.delete("/:id", deleteAccountById);
+
+//GET by email
+router.get("/", getAccountByEmail);
+
+module.exports = router;
